8d7b4bc747609404f16e861959cdfe2fd0e1b0e3
Robert J <robert@scriptmyjob.com>
2019-02-06 14:16:33 -0600

    Added cloudtrail.

---
+++ Terraform/s3.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "foo" {
+    bucket        = "logs_${data.aws_iam_account_alias.current.account_alias}"
+    force_destroy = true
+
+    policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::tf-test-trail"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::tf-test-trail/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
