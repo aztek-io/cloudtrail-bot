055f1c3bbc7a739fa41b13630149a2a39450eeb1
Robert J <robert@scriptmyjob.com>
2019-02-06 19:40:33 -0600

    Corrected bucket ACL.

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -7,22 +7,22 @@ resource "aws_s3_bucket" "foo" {
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Sid": "AWSCloudTrailAclCheck",
+            "Sid": "",
             "Effect": "Allow",
             "Principal": {
               "Service": "cloudtrail.amazonaws.com"
             },
             "Action": "s3:GetBucketAcl",
-            "Resource": "arn:aws:s3:::tf-test-trail"
+            "Resource": "arn:aws:s3:::logs.${data.aws_iam_account_alias.current.account_alias}"
         },
         {
-            "Sid": "AWSCloudTrailWrite",
+            "Sid": "",
             "Effect": "Allow",
             "Principal": {
               "Service": "cloudtrail.amazonaws.com"
             },
             "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::tf-test-trail/*",
+            "Resource": "arn:aws:s3:::logs.${data.aws_iam_account_alias.current.account_alias}/*",
             "Condition": {
                 "StringEquals": {
                     "s3:x-amz-acl": "bucket-owner-full-control"
