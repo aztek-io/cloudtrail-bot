a44099f425e2346933f66f877aa6fea474103c94
Robert J <robert@scriptmyjob.com>
2019-02-08 10:32:22 -0600

    Added logic to parse s3 objects.

--- Resources/Scripts/CloudTrailBot/cloudtrail_bot.py
+++ Resources/Scripts/CloudTrailBot/cloudtrail_bot.py
@@ -4,8 +4,10 @@ import logging
 import os
 import sys
 import json
+import boto3
 import re
 import requests
+import gzip
 
 #######################################
 ### Logging Settings ##################
@@ -13,6 +15,12 @@ import requests
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+#######################################
+### Boto3 Configs #####################
+#######################################
+
+s3 = boto3.client('s3')
 
 #######################################
 ### Global Vars #######################
@@ -38,23 +46,57 @@ def main(event, context):
 
     logger.info(ignore_list)
 
-    note_worthy_events = parse_event(event, ignore_list)
+    bucket, s3_object   = parse_event(event)
+    cloudtrail_event    = get_object_contents(bucket, s3_object)
+    note_worthy_events  = parse_cloudtrail_event(cloudtrail_event, ignore_list)
 
     for n in note_worthy_events:
         payload = create_slack_payload(n)
         post_to_slack(payload)
 
 
+#######################################
+### Boto3 Configs #####################
+#######################################
+
 def error(message, code=1):
     logger.error(message)
     sys.exit(code)
 
 
-def parse_event(event, ignore_list):
+#######################################
+### Program Specific Functions ########
+#######################################
+
+def parse_event(event):
+    bucket = event['s3']['bucket']['name']
+    s3_object = event['s3']['object']['key']
+
+    return bucket, s3_object
+
+
+def get_object_contents(bucket, s3_object):
+    logger.info('Pulling Data from s3. for {}/{}'.format(bucket, s3_object))
+
+    resp = s3.get_object(
+        Bucket=bucket,
+        Key=s3_object
+    )
+
+    body = resp['Body'].read()
+
+    contents = json.loads(gzip.decompress(body))
+
+    logger.info('Object retrieved: {}'.format(json.dumps(contents, indent=4)))
+
+    return contents
+
+
+def parse_cloudtrail_event(cloudtrail_event, ignore_list):
     note_worthy_events = list()
 
-    for event in event['Records']:
-        simplified_event = create_simplified_event(event)
+    for cloudtrail_event in cloudtrail_event['Records']:
+        simplified_event = create_simplified_event(cloudtrail_event)
 
         ignore_logic = None
 
@@ -77,18 +119,22 @@ def parse_event(event, ignore_list):
     return note_worthy_events
 
 
-def create_simplified_event(event):
+def create_simplified_event(cloudtrail_event):
     try:
-        user = event['userIdentity']['principalId']
+        user = cloudtrail_event['userIdentity']['principalId']
     except KeyError:
-        user = event['userIdentity']['invokedBy']
+        user = cloudtrail_event['userIdentity']['invokedBy']
 
     try:
-        action      = event['eventName']
-        event_time  = event['eventTime']
-        resources   = event['resources']
+        resources   = cloudtrail_event['resources']
     except KeyError:
-        error('Parsing error: {}'.format(json.dumps(event, indent=4)))
+        resources   = cloudtrail_event['requestParameters']
+
+    try:
+        action      = cloudtrail_event['eventName']
+        event_time  = cloudtrail_event['eventTime']
+    except KeyError:
+        error('Parsing error: {}'.format(json.dumps(cloudtrail_event, indent=4)))
 
     simplified_event = {
         'invokedBy': user,
@@ -173,34 +219,18 @@ if __name__ == '__main__':
     logger.addHandler(stream)
 
     event = {
-        "Records":[
-            {
-                "userIdentity":{
-                    "type":"AWSService",
-                    "invokedBy":"autoscaling.amazonaws.com"
-                },
-                "eventTime":"2019-02-07T01:29:57Z",
-                "eventSource":"sts.amazonaws.com",
-                "eventName":"AssumeRole",
-                "awsRegion":"us-west-2",
-                "sourceIPAddress":"autoscaling.amazonaws.com",
-                "userAgent":"autoscaling.amazonaws.com",
-                "requestParameters":{
-                    "roleArn":"arn:aws:iam::123456789012:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-                    "roleSessionName":"AutoScaling",
-                    "durationSeconds":1800
-                },
-               "resources":[
-                    {
-                        "ARN":"arn:aws:iam::123456789012:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
-                        "accountId":"123456789012",
-                        "type":"AWS::IAM::Role"
-                    }
-                ],
-                "recipientAccountId":"123456789012",
-                "sharedEventID":"d910b38d-8a76-4568-9a8f-789a63b722d6"
-            }
-        ]
+        "awsRegion": "us-west-2",
+        "eventTime": "2019-02-08T13:46:44.679Z",
+        "eventName": "ObjectCreated:Put",
+        "s3": {
+            "bucket": {
+                "name": "security.aztek.logs",
+                "arn": "arn:aws:s3:::security.aztek.logs"
+            },
+            "object": {
+                "key": "prefix/AWSLogs/976168295228/CloudTrail/us-west-2/2019/02/08/976168295228_CloudTrail_us-west-2_20190208T1350Z_GfAMIWcKag0WzGeB.json.gz"
+           }
+        }
     }
 
     main(event, None)
