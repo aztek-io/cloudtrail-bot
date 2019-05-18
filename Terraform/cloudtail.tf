d5451c47ae64ad3a7102f0c3a80fe199764bae45
Robert J <robert_jackson1@dell.com>
2019-05-18 15:02:50 -0500

    Enabled log file validation.

--- Terraform/cloudtail.tf
+++ Terraform/cloudtail.tf
@@ -3,6 +3,7 @@ resource "aws_cloudtrail" "cloudtrail_bot" {
     s3_bucket_name                = "${aws_s3_bucket.cloudtrail_logs.id}"
     s3_key_prefix                 = "prefix"
     include_global_service_events = false
+    enable_log_file_validation    = true
 
     tags {
         Application = "${lookup(var.global,"application")}"
